4523bc4266a4a70aadc3a0e509ee4b1cde55a076
Praise <oyeniyipraise@gmail.com>
2025-05-05 23:38:36 +0100

    responsiveness

--- src/app/components/aboutPage/Revolution.jsx
+++ src/app/components/aboutPage/Revolution.jsx
@@ -6,7 +6,7 @@ const Revolution = () => {
     <div className="w-full">
         <div className='mx-auto w-[90%] md:w-5/6'>
             <div className="flex flex-col md:flex-row gap-y-5 w-full justify-between items-center gap-x-14">
-                <div className="text space-y-5 md:selection:w-3/6 order-2 md:order-1">
+                <div className="text space-y-5 md:w-3/6 order-2 md:order-1">
                     <h3 className='section-head'>From Campus Challenge To Housing Revolution</h3>
                     <p className='section-para'>At Find-a-Home FUTA, we simplify the off-campus housing journey for FUTA students. Our platform connects you with verified properties and reliable service providers, ensuring a seamless experience. Discover affordable essentials tailored for student life, all in one place.</p>
                 </div>
